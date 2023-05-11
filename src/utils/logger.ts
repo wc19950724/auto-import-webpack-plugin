5d069a6d51ceebc6dd80693a69112f0b9cc95996
王超 <wangchao1.vendor@senseauto.com>
2023-05-11 18:12:56 +0800

    fix:  logLevel

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,42 +4,42 @@ import { getOptions } from "@/common";
 
 function error(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error"].indexOf(logLevel) > -1) {
+  if (["error", "warn", "info"].indexOf(logLevel) > -1) {
     console.log(chalk.redBright(...args));
   }
 }
 
 function warn(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error", "warn"].indexOf(logLevel) > -1) {
+  if (["warn", "info"].indexOf(logLevel) > -1) {
     console.log(chalk.yellowBright(...args));
   }
 }
 
 function info(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error", "warn", "info"].indexOf(logLevel) > -1) {
+  if (["info"].indexOf(logLevel) > -1) {
     console.log(chalk.blueBright(...args));
   }
 }
 
 function success(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error", "warn", "info"].indexOf(logLevel) > -1) {
+  if (["info"].indexOf(logLevel) > -1) {
     console.log(chalk.greenBright(...args));
   }
 }
 
 function log(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error", "warn", "info"].indexOf(logLevel) > -1) {
+  if (["info"].indexOf(logLevel) > -1) {
     console.log(...args);
   }
 }
 
 function path(...args: any[]) {
   const { logLevel } = getOptions();
-  if (["error", "warn", "info"].indexOf(logLevel) > -1) {
+  if (["info"].indexOf(logLevel) > -1) {
     console.log(chalk.gray(...args));
   }
 }
